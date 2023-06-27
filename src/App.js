e0dcbacf88af4ae16868e36a5af4cf6e622920fd
oscar-fernandez <68928859+oscar-fernandez@users.noreply.github.com>
2023-06-27 14:10:16 -0400

    Create featured component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
-import logo from './logo.svg';
+import { Featured } from './components/Featured.tsx';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img
-          src={'https://i.imgur.com/KKR9P1z.jpg'}
-          className='App-logo'
-          alt='logo'
-        />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <Featured />
     </div>
   );
 }
---
+++ src/components/Featured.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Featured.css';
+
+export const Featured = () => {
+  return (
+    <div className="featured">
+      <a href="#" className='image-one'><div></div></a>
+      <a href="#" className='image-two'><div></div></a>
+      <a href="#" className='image-three'><div></div></a>
+      <a href="#" className='image-four'><div></div></a>
+      <a href="#" className='image-five'><div></div></a>
+    </div>
+  )
+}
