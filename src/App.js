0fea554d05027dcd4fac40f696d402d40b2bf184
Spruill <959310@cognizant.com>
2023-06-27 14:48:25 -0400

    added footer component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Footer from "./Footer/Footer";
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
+    <div className="App">
+      <header className="App-header">
         <img
-          src={'https://i.imgur.com/KKR9P1z.jpg'}
-          className='App-logo'
-          alt='logo'
+          src={"https://i.imgur.com/KKR9P1z.jpg"}
+          className="App-logo"
+          alt="logo"
         />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
         >
           Learn React
         </a>
       </header>
+      <Footer />
     </div>
   );
 }
