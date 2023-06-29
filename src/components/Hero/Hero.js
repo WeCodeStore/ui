b50b8d447e22ecbab94242ce340d71058cf581f3
Manvir <manvir@Manvirs-MBP.lan>
2023-06-29 11:27:26 -0400

    adding feature/hero

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./Hero.css";
+
+const HeroImageGrid = () => {
+   return(
+    <div class="container-hero">
+      <h2>Large text</h2>  
+      <h3>
+        Small text
+      </h3>
+      <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        {/* <!-- Indicators --> */}
+        <ol class="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+          <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+    
+        {/* <!-- Wrapper for slides --> */}
+        <div class="carousel-inner">
+          <div class="item active">
+            <img src="https://imgur.com/D1ouCU1.jpeg" alt="Outdoor" />
+          </div>
+    
+          <div class="item">
+            <img src="https://imgur.com/tN1h4ou.jpeg" alt="Living" />
+          </div>
+        
+          <div class="item">
+            <img src="https://imgur.com/FhEh9GT.jpeg" alt="Bedroom" />
+          </div>
+        </div>
+    
+        {/* <!-- Left and right controls --> */}
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+          <span class="glyphicon glyphicon-chevron-left"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+          <span class="glyphicon glyphicon-chevron-right"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </div>
+)};
+
+
+export default HeroImageGrid;
--- src/components/HomeViewComponent.jsx
+++ src/components/HomeViewComponent.jsx
@@ -1,12 +1,12 @@
-
+import HeroImageGrid from  './Hero/Hero';
 const HomeViewComponent = () => {
     return (
         <div className="home-view-container" data-testid="home-view-container">
-            <div className="hero-section-container" data-testid="hero-section-container">Hero Section</div>
+            <div className="hero-section-container" data-testid="hero-section-container"> <HeroImageGrid /></div>
             <div className="shop-by-category-container" data-testid="shop-by-category-container">Shop By Category</div>
             <div className="featured-container" data-testid="featured-container">Featured</div>
         </div>
     );
 }
 
-export default HomeViewComponent
+export default HomeViewComponent;
