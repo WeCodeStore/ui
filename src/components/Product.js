8e1738acb362f342b2e1ace35b10a2a06dd80f33
Spruill <959310@cognizant.com>
2023-07-06 15:13:14 -0400

    styled shop view container

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,13 @@
 export default function Product({ product, rating }) {
   return (
     <div>
+      <img
+        src={product.image}
+        style={{ width: "250px", height: "250px" }}
+        className="productImage"
+      />
       <p>{product.name}</p>
       <p>{product.price}</p>
-      <p>{product.image}</p>
       <p>{product.description}</p>
       <p>{product.category}</p>
       <p>{rating}</p>
