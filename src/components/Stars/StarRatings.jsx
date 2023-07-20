8977c88c836d6c000266a511ec0c541942579063
oscar-fernandez <68928859+oscar-fernandez@users.noreply.github.com>
2023-07-20 11:22:26 -0400

    Update ProductPage component

--- src/components/Stars/StarRatings.jsx
+++ src/components/Stars/StarRatings.jsx
@@ -1,11 +1,13 @@
-import ReactStars from "react-stars";
-import "./StarRatings.css";
+import ReactStars from 'react-stars';
+import './StarRatings.css';
 
-const StarRatings = ({ numStars = 5, rate, reviews }) => {
+const StarRatings = (props) => {
+  const { numStars = 5, rate, reviews } = props;
+
   return (
-    <div className="star-rating" data-testid="starRatingId">
+    <div className='star-rating' data-testid='starRatingId'>
       <ReactStars count={numStars} value={rate} size={24} edit={false} />
-      <span className="star-rating-span">({reviews})</span>
+      <span className='star-rating-span'>{reviews ? `(${reviews})` : ''}</span>
     </div>
   );
 };
