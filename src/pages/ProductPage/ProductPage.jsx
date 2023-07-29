18c79777dba754e3e614d0fef4d555c6b676b2a6
PTsishuk <tishukp@gmail.com>
2023-07-28 22:43:04 -0400

    added react persistance on refresh

--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -1,21 +1,20 @@
-import React, { useState, useRef, useEffect } from 'react';
-import StarRatings from '../../components/Stars/StarRatings';
-import { useSelector, useDispatch } from 'react-redux';
-import { FaChevronRight } from 'react-icons/fa';
-import { Link } from 'react-scroll';
-import './ProductPage.css';
-import { addItemToCart } from '../../store/cartSlice';
+import React, { useState, useRef } from "react";
+import StarRatings from "../../components/Stars/StarRatings";
+import { useSelector, useDispatch } from "react-redux";
+import { FaChevronRight } from "react-icons/fa";
+import { Link } from "react-scroll";
+import "./ProductPage.css";
+import { addItemToCart } from "../../store/cartSlice";
 
 export default function ProductPage() {
   const dispatch = useDispatch();
   const product = useSelector((state) => state.actions.product);
   const reviews = useRef([]);
-  const imageRef = useRef(product.images[0]);
   const [currentImage, setCurrentImage] = useState(product.images[0]);
   const [showReviews, setShowReviews] = useState(false);
 
   const cartQ = 1;
-  
+
   const getReviews = async () => {
     if (product.totalReviews > 0) {
       const data = await fetch(
@@ -39,31 +38,31 @@ export default function ProductPage() {
   };
 
   const addToCart = () => {
-    dispatch(addItemToCart([product, cartQ]))
-  }
+    dispatch(addItemToCart([product, cartQ]));
+  };
 
   return (
-    <div className='ProductPage'>
-      <div className='product-container'>
-        <div className='product-container--image-selector'>
+    <div className="ProductPage">
+      <div className="product-container">
+        <div className="product-container--image-selector">
           {product.images.map((image, idx) => (
             <img
               key={idx}
-              className='selected'
+              className="selected"
               src={image}
               onClick={(e) => selectImage(e)}
-              alt=''
+              alt=""
             />
           ))}
         </div>
-        <div className='product-container--image'>
-          <img src={currentImage} alt='Graphite Sheets' />
+        <div className="product-container--image">
+          <img src={currentImage} alt="Graphite Sheets" />
         </div>
-        <div className='product-container--info'>
-          <h2 className='product-container--name'>{product.name}</h2>
+        <div className="product-container--info">
+          <h2 className="product-container--name">{product.name}</h2>
           <Link
-            activeClass='active'
-            to='reviews-section'
+            activeClass="active"
+            to="reviews-section"
             spy={true}
             smooth={true}
             duration={100}
@@ -74,21 +73,23 @@ export default function ProductPage() {
               reviews={product.totalReviews}
             />
           </Link>
-          <p className='product-container--price'>{`$${product.price.toFixed(
+          <p className="product-container--price">{`$${product.price.toFixed(
             2
           )}`}</p>
-          <p className='product-container--description'>
+          <p className="product-container--description">
             {product.description}
           </p>
-          <button className='button button-blue' onClick={addToCart}>Add to Cart</button>
-          <div className='product-details'>
-            <div className='product-details--menu'>
+          <button className="button button-blue" onClick={addToCart}>
+            Add to Cart
+          </button>
+          <div className="product-details">
+            <div className="product-details--menu">
               <h6>Details</h6>
               <h6>Care</h6>
               <h6>Returns</h6>
               <h6>FAQ</h6>
             </div>
-            <div className='product-details--description'>
+            <div className="product-details--description">
               <p>
                 Ut enim ad minim veniam, quis nostrud exercitation ullamco
                 laboris nisi ut aliquip ex ea commodo consequat.Ut enim ad minim
@@ -99,7 +100,7 @@ export default function ProductPage() {
                 exercitation ullamco laboris nisi ut aliquip ex ea commodo
                 consequat.
               </p>
-              <ul className='product-details--list'>
+              <ul className="product-details--list">
                 <li>100% long-staple cotton</li>
                 <li>480 thread count</li>
                 <li>
@@ -111,25 +112,25 @@ export default function ProductPage() {
           </div>
         </div>
       </div>
-      <div className='reviews-section' name='reviews-section'>
+      <div className="reviews-section" name="reviews-section">
         <h3>
-          Reviews{' '}
+          Reviews{" "}
           <FaChevronRight
-            className={showReviews ? 'chev rotated' : 'chev'}
+            className={showReviews ? "chev rotated" : "chev"}
             onClick={getReviews}
           />
         </h3>
-        <div className={showReviews ? 'reviews-container' : 'hide'}>
+        <div className={showReviews ? "reviews-container" : "hide"}>
           {reviews.current.length > 0 ? (
             reviews.current.map((review, idx) => (
-              <div key={idx} className='review-row'>
-                <p className='user'>{review.userId} User</p>
-                <p className='comment'>{review.comment}</p>
+              <div key={idx} className="review-row">
+                <p className="user">{review.userId} User</p>
+                <p className="comment">{review.comment}</p>
                 <StarRatings rate={review.rating} />
               </div>
             ))
           ) : (
-            <div className='empty-row'>
+            <div className="empty-row">
               <p>There are no reviews...</p>
             </div>
           )}
--- src/store.jsx
+++ src/store.jsx
@@ -2,15 +2,37 @@ import { configureStore } from "@reduxjs/toolkit";
 import actionsReducer from "./store/actionSlice";
 import cartReducer from "./store/cartSlice";
 import { apiSlice } from "./store/apiSlice";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "redux";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const reducers = combineReducers({
+  actions: actionsReducer,
+  cart: cartReducer,
+  [apiSlice.reducerPath]: apiSlice.reducer,
+});
+
+const persistedReducers = persistReducer(persistConfig, reducers);
 
 export default configureStore({
-  reducer: {
-    actions: actionsReducer,
-    cart: cartReducer,
-    [apiSlice.reducerPath]: apiSlice.reducer,
-  },
+  reducer: persistedReducers,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: false,
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
     }).concat(apiSlice.middleware),
 });
